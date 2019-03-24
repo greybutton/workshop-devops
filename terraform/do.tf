fd58a1d235cfe482679b433f951b081470e0b7a3
greybutton <greybutton@yandex.com>
2019-03-24 20:00:00 +0800

    terraform

---
+++ terraform/do.tf
@@ -0,0 +1,10 @@
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "web" {
+  image  = "ubuntu-18-04-x64"
+  name   = "web-1"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+}
